62046c4607a8be6b897dc089e3c47b15f231b195
Bach Ngo <bach.ngb@gmail.com>
2026-04-14 17:56:20 +0930

    Change --green to 'yellow' in variables

--- src/styles/PrismStyles.js
+++ src/styles/PrismStyles.js
@@ -5,7 +5,7 @@ const prismColors = {
   lineHighlight: `#1d2d50`,
   blue: `#5ccfe6`,
   purple: `#c3a6ff`,
-  green: `#cf8d2e`,
+  green: `#bae67e`,
   yellow: `#ffd580`,
   orange: `#ffae57`,
   red: `#ef6b73`,
