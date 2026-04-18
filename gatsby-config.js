e7d652b94dd7da8e17cfa57f69e8345f20541bf5
Bach Ngo <bach.ngb@gmail.com>
2026-04-18 19:59:52 +0930

    Attempt to update the page title

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,9 +2,9 @@ const config = require('./src/config');
 
 module.exports = {
   siteMetadata: {
-    title: 'Brittany Chiang',
+    title: 'Bach Ngo',
     description:
-      'Brittany Chiang is a software engineer who specializes in building (and occasionally designing) exceptional digital experiences.',
+      'Bach Ngo is a software engineer who specializes in building (and occasionally designing) exceptional digital experiences.',
     siteUrl: 'https://brittanychiang.com', // No trailing slash allowed!
     image: '/og.png', // Path to your image you placed in the 'static' folder
     twitterUsername: '@bchiang7',
@@ -20,8 +20,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'Brittany Chiang',
-        short_name: 'Brittany Chiang',
+        name: 'Bach Ngo',
+        short_name: 'Bach Ngo',
         start_url: '/',
         background_color: config.colors.darkNavy,
         theme_color: config.colors.navy,
